816ae56d375ad6fd8cd74b8a113dd38b9b42296e
MiroslavCharakchiev <fearfax2@yahoo.com>
2017-04-12 16:51:50 +0300

    -Add AutoMigrations

---
+++ Eventis/Migrations/201704121338141_InitialCreate.cs
@@ -0,0 +1,184 @@
+namespace Eventis.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 15),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(nullable: false, maxLength: 200),
+                        Author_Id = c.String(nullable: false, maxLength: 128),
+                        Event_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.Author_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Events", t => t.Event_Id, cascadeDelete: true)
+                .Index(t => t.Author_Id)
+                .Index(t => t.Event_Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.Events",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 100),
+                        Content = c.String(nullable: false, maxLength: 100),
+                        ImagePath = c.String(),
+                        ViewCount = c.Int(nullable: false),
+                        StartDate = c.DateTime(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                        AuthorId = c.String(maxLength: 128),
+                        Status = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.AuthorId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId)
+                .Index(t => t.AuthorId);
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        PhoneNumber = c.String(),
+                        EmailAddress = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.ContactEvents",
+                c => new
+                    {
+                        Contact_Id = c.Int(nullable: false),
+                        Event_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Contact_Id, t.Event_Id })
+                .ForeignKey("dbo.Contacts", t => t.Contact_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Events", t => t.Event_Id, cascadeDelete: true)
+                .Index(t => t.Contact_Id)
+                .Index(t => t.Event_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Comments", "Event_Id", "dbo.Events");
+            DropForeignKey("dbo.ContactEvents", "Event_Id", "dbo.Events");
+            DropForeignKey("dbo.ContactEvents", "Contact_Id", "dbo.Contacts");
+            DropForeignKey("dbo.Events", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.Events", "AuthorId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Comments", "Author_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.ContactEvents", new[] { "Event_Id" });
+            DropIndex("dbo.ContactEvents", new[] { "Contact_Id" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Events", new[] { "AuthorId" });
+            DropIndex("dbo.Events", new[] { "CategoryId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Comments", new[] { "Event_Id" });
+            DropIndex("dbo.Comments", new[] { "Author_Id" });
+            DropTable("dbo.ContactEvents");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Events");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Comments");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ Eventis/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Eventis.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Eventis.Models.IdentityConfig.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Eventis.Models.IdentityConfig+ApplicationDbContext";
+        }
+
+        protected override void Seed(Eventis.Models.IdentityConfig.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
