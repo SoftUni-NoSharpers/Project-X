31df3c8cc0f086a98f8daa9d5a0f64b47544db00
PhilipYordanov <fjordanov1@gmail.com>
2017-04-12 18:24:20 +0300

    -AutoMigrations  = True;

--- Eventis/Migrations/Configuration.cs
+++ Eventis/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Eventis.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Eventis.Models.IdentityConfig.ApplicationDbContext context)
