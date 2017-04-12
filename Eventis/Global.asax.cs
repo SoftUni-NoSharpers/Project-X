99d007d9949a28e3fd7a054521f4c17acb7f28e2
PhilipYordanov <fjordanov1@gmail.com>
2017-04-12 18:21:33 +0300

    -MIgrations

--- Eventis/Global.asax.cs
+++ Eventis/Global.asax.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
+using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using static Eventis.Models.IdentityConfig;
 
 namespace Eventis
 {
