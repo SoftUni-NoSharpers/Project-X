c8d7a2decffacd0549d24c9703484b48e280e301
PhilipYordanov <fjordanov1@gmail.com>
2017-04-12 15:55:47 +0300

    -Initial Empty Project

---
+++ Eventis/Eventis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eventis.Startup))]
+namespace Eventis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
