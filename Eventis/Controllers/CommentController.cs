38fe9129562666639fa0421536ca924a1a47b29d
PhilipYordanov <fjordanov1@gmail.com>
2017-04-25 14:18:51 +0300

    - implemented comment posting

---
+++ Eventis/Controllers/CommentController.cs
@@ -0,0 +1,44 @@
+﻿using Eventis.Models.Eventis;
+using Eventis.Models.Identity;
+using System.Web.Mvc;
+using System.Net;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Eventis.Models.Eventis.ViewModels;
+
+namespace Eventis.Controllers
+{
+    public class CommentController : Controller
+    {
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CommentViewModel model)
+        {
+            //if (model.EventId == null)
+            //{
+            //    return RedirectToAction("Index", "Home");
+            //}
+
+            var loggedInUserId = this.User.Identity.GetUserId();
+
+            var comment = new Comment()
+            {
+                Content = model.Content,
+                AuthorId = loggedInUserId,
+                EventId = model.EventId
+            };
+
+            if (model.Content == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "You must enter some content.");
+            }
+
+            var db = new ApplicationDbContext();
+
+            db.Comments.Add(comment);
+            db.SaveChanges();
+
+            return RedirectToAction("Details", "Events", new { Id = model.EventId });
+        }
+    }
+}
---
+++ Eventis/Models/Eventis/ViewModels/CommentViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Eventis.Models.Eventis.ViewModels
+{
+    public class CommentViewModel
+    {
+        public string AuthorId { get; set; }
+
+        public string Content { get; set; }
+
+        public int EventId { get; set; }
+    }
+}
